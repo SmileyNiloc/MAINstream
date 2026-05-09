8f97d2875d3e39e2fb8e236334b17387ba331163
Colin Aten <caten@udel.edu>
2026-05-09 18:03:02 +0000

    added various new free models

--- main.py
+++ main.py
@@ -19,10 +19,22 @@ if not GEMINI_API_KEY:
         "GEMINI_API_KEY not found. Add it to .env as GEMINI_API_KEY=... or export it in your shell."
     )
 main_llm_handler.add_api(geminiApi(GEMINI_API_KEY, name="Gemini API"))
-main_llm_handler.add_api(openRouterApi(
-    OPEN_ROUTER_API_KEY, name="OpenRouter API"))
-main_llm_handler.add_api(openRouterApi(
-    OPEN_ROUTER_API_KEY, name="NVIDIA nemotron", model="nvidia/nemotron-3-super-120b-a12b:free"))
+
+open_router_models = {
+    "Gemini Gemma 4 31B": "google/gemma-4-31b-it:free",
+    "NVIDIA nemotron": "nvidia/nemotron-3-super-120b-a12b:free",
+    "Poolside Laguna M.1": "poolside/laguna-m.1:free",
+    "Owl Alpha": "openrouter/owl-alpha",
+    "Baidu Qianfan: CoBuddy": "baidu/cobuddy:free",
+    "OpenAI: gpt-oss-120b": "openai/gpt-oss-120b:free",
+    #    "Anthropic: Claude Haiku 4.5": "anthropic/claude-haiku-4.5" # This one is not free
+
+}
+
+for model_name, model_id in open_router_models.items():
+    main_llm_handler.add_api(openRouterApi(
+        OPEN_ROUTER_API_KEY, name=model_name, model=model_id))
+
 # if OPENAI_API_KEY:
 # main_llm_handler.add_api(openaiApi(OPENAI_API_KEY))
 app = App(llm_handler=main_llm_handler, database_manager=database_manager)
